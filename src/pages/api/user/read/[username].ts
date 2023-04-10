5f88520fc487933031d13b07283660fbdfab0796
yousaf wazir <umbrellaman612@gmail.com>
2023-04-10 12:46:03 +0100

    added read user details

---
+++ src/pages/api/user/read/[username].ts
@@ -0,0 +1,68 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/db";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  if (method !== "GET") {
+    res.setHeader("Allow", "GET");
+    res.status(405).json({
+      error: {
+        code: 405,
+        message: `Method ${method} not allowed. Please use the GET method instead.`,
+      },
+    });
+    console.error(
+      `Method ${method} not allowed on this route /api/user/read/[username]`
+    );
+    return;
+  }
+
+  const { username }: any = req.query;
+
+  if (!username) {
+    res.status(400).json({
+      error: {
+        code: 400,
+        message: "Missing username in request url",
+      },
+    });
+    return;
+  }
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        username: username,
+      },
+      select: {
+        username: true,
+        posts: true,
+      },
+    });
+
+    if (!user) {
+      res.status(404).json({
+        error: {
+          code: 404,
+          message: `User with username "${username}" not found`,
+        },
+      });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: {
+        code: 500,
+        message: "Error fetching user",
+      },
+    });
+  }
+}
