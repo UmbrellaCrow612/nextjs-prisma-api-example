81f799fc9339cd277af784440bd3cf0be1e57b00
yousaf wazir <umbrellaman612@gmail.com>
2023-04-10 11:43:42 +0100

    added basic validation for username and pass

--- src/pages/api/user/create.ts
+++ src/pages/api/user/create.ts
@@ -2,9 +2,19 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const method = req.method;
+  const { username, password } = req.body;
 
   if (method === "POST") {
-    // Handle the POST request
+    if (!username || !password) {
+      res.status(400).json({
+        error: {
+          code: 400,
+          message: "Missing username and or password in request body",
+        },
+      });
+    } else {
+      res.status(200).json({ created: `${username} + ${password}` });
+    }
   } else {
     res.setHeader("Content-Type", "application/json");
     res.setHeader("Allow", "POST");
