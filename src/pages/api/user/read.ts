59628683d605e035e54ef1254cba8165f3f1501c
yousaf wazir <umbrellaman612@gmail.com>
2023-04-10 12:06:03 +0100

    started adding read

--- src/pages/api/user/read.ts
+++ src/pages/api/user/read.ts
@@ -1,1 +1,22 @@
-export {}
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method, body } = req;
+
+  if (method !== "GET") {
+    res.setHeader("Allow", "POST");
+    res.status(405).json({
+      error: {
+        code: 405,
+        message: `Method ${method} not allowed. Please use the POST method instead.`,
+      },
+    });
+    console.error(
+      `Method ${method} not allowed on this route /api/user/create`
+    );
+    return;
+  }
+}
