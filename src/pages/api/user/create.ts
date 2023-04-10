fd796ea6ae02926dac44879f70efc71ba19be178
yousaf wazir <umbrellaman612@gmail.com>
2023-04-10 11:15:45 +0100

    starting to add prod create user api

--- src/pages/api/user/create.ts
+++ src/pages/api/user/create.ts
@@ -1,1 +1,18 @@
-export {}
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const method = req.method;
+
+  if (method === "POST") {
+    // Handle the POST request
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Allow", "POST");
+    res.status(405).json({
+      error: {
+        code: 405,
+        message: `Method ${method} not allowed`,
+      },
+    });
+  }
+}
