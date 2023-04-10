988ba4133de29086c49a6125fbea71b5e37dfa7c
yousaf wazir <umbrellaman612@gmail.com>
2023-04-10 11:56:01 +0100

    added create user

--- src/pages/api/user/create.ts
+++ src/pages/api/user/create.ts
@@ -1,22 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/db";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const method = req.method;
-  const { username, password } = req.body;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method, body } = req;
 
-  if (method === "POST") {
-    if (!username || !password) {
-      res.status(400).json({
-        error: {
-          code: 400,
-          message: "Missing username and or password in request body",
-        },
-      });
-    } else {
-      res.status(200).json({ created: `${username} + ${password}` });
-    }
-  } else {
-    res.setHeader("Content-Type", "application/json");
+  if (method !== "POST") {
     res.setHeader("Allow", "POST");
     res.status(405).json({
       error: {
@@ -25,7 +16,47 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       },
     });
     console.error(
-      `Method ${method} not allowed on this route /api/user/create `
+      `Method ${method} not allowed on this route /api/user/create`
     );
+    return;
+  }
+
+  const { username, password } = body;
+
+  if (!username || !password) {
+    res.status(400).json({
+      error: {
+        code: 400,
+        message: "Missing username and/or password in request body",
+      },
+    });
+    return;
+  }
+
+  try {
+    const existingUser = await prisma.user.findFirst({
+      where: { username },
+    });
+
+    if (existingUser) {
+      res.status(409).json({
+        error: {
+          code: 409,
+          message: `Username "${username}" already exists`,
+        },
+      });
+      return;
+    }
+
+    const user = await prisma.user.create({ data: { username, password } });
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: {
+        code: 500,
+        message: "Error creating user",
+      },
+    });
   }
 }
